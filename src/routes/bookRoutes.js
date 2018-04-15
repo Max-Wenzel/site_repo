e9611c7d4c4f16de6c4fbb2a27211026f1b42550
Israel Miles <ismi7509@colorado.edu>
2018-04-14 23:10:13 -0600

    asynchronous functionality and showing sql table on dashboard

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,59 +5,55 @@ const sql = require('mssql');
 const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
-	// hard coded book database to be replaced by MySQL.
-	// use const for variables that do not change.
-	// this needs to be changed so that we are getting CA/TA info from SQL
-	const dashboard=[
-		{
-			title: 'War and Peace',
-			genre: 'Historical Fiction',
-			author: 'Lev Nikolayevich Tolstoy'
-		},
-		{
-			title: 'Les Miserables',
-			genre: 'Historical Fiction',
-			author: 'Victor Hugo'
-		},
-		{
-			title: 'Childhood',
-			genre: 'Biography',
-			author: 'Lev Nikolayevich Tolstoy'
-		}
-	];
 
 	// routing to view the homepage
 	bookRouter.route('/')
 		.get((req, res) => {
-			const request = new sql.Request();
+/*
+	Inside a function marked as async, you are allowed to place the await keyword
+	in front of an expression that returns a promise. When you do, the execution of
+	the async function is paused until the promise is resolved.
+	Asynchronous Definition: of two or more events not existing or happening at
+	the same time.
+*/
+			// Immediately Invoked Function Expression (IIFE)
+			// There is a lot going on right here. Look up the concepts!
+			(async function query() {
 
-			// this returns a promise that sends back some desired result
-			// but also includes error handling
-			request.query('SELECT * FROM assistants')
-				.then((result) => {
-					debug(result);
-					res.render(
-						'dashboard', {
-							nav,
-					  		title: 'SiTE',
-					  		// this is the sql table
-					  		dashboard: result.recordset
-					  	}
-					);
-				});
+				const request = new sql.Request();
+				// this returns a promise that sends back some desired result
+				// but also includes error handling
+				const result = await request.query('SELECT * FROM assistants')
+
+				res.render(
+					'dashboard', {
+						nav,
+				  		title: 'SiTE',
+				  		// this is the sql table
+				  		dashboard: result.recordset
+				  	}
+				);
+			}());
 		});
 
 	// routing to view a single assistant
 	bookRouter.route('/:id')
 		.get((req, res) => {
-			const id = req.params.id;
-			res.render(
-				'assistant', {
-					nav,
-			  		title: 'SiTE',
-			  		book: dashboard[id]
-		  		}
-			);
+			(async function query(){
+				
+				const id = req.params.id;
+				const request = new sql.Request();
+				const result = 
+					await request.input('id', sql.Int, id)
+						.query('SELECT * FROM assistants WHERE id = @id');
+				res.render(
+					'assistant', {
+						nav,
+				  		title: 'SiTE',
+				  		dashboard: result.recordset[0]
+			  		}
+				);
+			}());
 		});
 	// now that we've created bookRouter, return it.
 	return bookRouter;
