ed7e3e38494df6df4eff5c3710090cb4dec19053
Israel Miles <ismi7509@colorado.edu>
2018-04-20 00:04:24 -0600

    sign in page for local user with additional work on strategy and user session

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const sql = require('mssql');
+const passport = require('passport');
 // pass in bookRoutes as argument for debug
 const debug = require('debug')('app:authRoutes');
 
 const authRouter = express.Router();
 
-function router() {
+function router(nav) {
 	authRouter.route('/signUp')
 		.post((req, res) => {
 
+			const { username, password } = req.body;
 			const request = new sql.Request();
 
 			(async function addUser(){
@@ -16,11 +18,12 @@ function router() {
 				try {
 					const results = await request.query('SELECT * FROM assistants')
 					debug('Connected correctly to server');
+					const user = { username, password };
+					debug(user)
 				} catch (err) {
 					debug(err);
 				}
 				// login and create user, fetched from signup submit button
-				debug(results);
 				req.login(results.ops[0], () => {
 					res.redirect('/auth/profile');
 				});
@@ -30,6 +33,17 @@ function router() {
 
 			res.json(req.body);
 		});
+	authRouter.route('/signin')
+		.get((req, res) => {
+			res.render('signin', {
+				nav,
+				title: 'Sign In'
+			});
+		})
+		.post(passport.authenticate('local', {
+			successRedirect: '/auth/profile',
+			failureRedirect: '/'
+		}));
 	// since the user is logged in, passport will attach the user to a request
 	authRouter.route('/profile')
 		.get((req, res) => {
